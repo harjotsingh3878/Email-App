c49fb35906d019791cb812b4ffece765204d00ff
Harjot Singh <harjotsingh538@gmail.com>
2018-02-10 16:53:48 -0500

    added files to get and create data from database

---
+++ src/CommentBox.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import CommentList from './CommentList';
+import CommentForm from './CommentForm';
+import DATA from './data';
+import './index.css';
+class CommentBox extends Component {
+ constructor(props) {
+ super(props);
+ this.state = { data: [] };
+ }
+ render() {
+ return (
+ <div className='commentBox'>
+ <h2>Comments:</h2>
+ <CommentList data={ DATA }/>
+ <CommentForm />
+ </div>
+ )
+ }
+}
+export default CommentBox;
